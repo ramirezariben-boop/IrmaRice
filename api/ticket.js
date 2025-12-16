6bb57a88325011faab01cc7b0d8e3f63197aef5f
AriiBen <ramirez.ariben@gmail.com>
2025-12-16 08:11:00 -0600

    Add SSL configuration to database connection

--- api/ticket.js
+++ api/ticket.js
@@ -2,7 +2,9 @@ import { Pool } from "pg";
 
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
+  ssl: { rejectUnauthorized: false },
 });
+
 
 // 💲 Precios unitarios
 const PRECIOS = {
